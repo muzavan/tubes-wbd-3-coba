7bb9470903b321c57204e27003384f45d79d3336
Muhammad Reza Irvanda <muzavan@gmail.com>
2014-12-17 09:45:31 +0700

    Ngerubah AddUser

--- src/java/blog/cobablog.java
+++ src/java/blog/cobablog.java
@@ -7,8 +7,6 @@ package blog;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,9 +21,6 @@ import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -37,15 +32,20 @@ public class cobablog {
     public static final String AUTH_TOKEN = "Jeqg6K2cgLB509zZQ2EMxr07qezh2CfActE23SoA";
     public static Boolean selesaiRetrieve = false;
     public static DataSnapshot data = null;
-    public static final String LINK_FIREBASE= "https://simple-blog-19.firebaseio.com/";
+    public static final String LINK_FIREBASE= "https://luminous-heat-1349.firebaseio.com/";
     public static Firebase fb = new Firebase(LINK_FIREBASE);
     /**
      * Web service operation
+     * @param judul
+     * @param konten
+     * @param tanggal
+     * @param author
+     * @return 
      */
     @WebMethod(operationName = "addPost")
     public Boolean addPost(@WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") String tanggal, @WebParam(name = "author") String author) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();
+        Map<String,Object> mp = new HashMap<>();
         
         mp.put("judul", judul);
         mp.put("konten", konten);
@@ -60,6 +60,7 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "listPost")
     public String listPost() {
@@ -86,11 +87,16 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idPost
+     * @param judul
+     * @param konten
+     * @param tanggal
+     * @return 
      */
     @WebMethod(operationName = "editPost")
     public Boolean editPost(@WebParam(name = "idPost") String idPost, @WebParam(name = "judul") String judul, @WebParam(name = "konten") String konten, @WebParam(name = "tanggal") Date tanggal) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();
+        Map<String,Object> mp = new HashMap<>();
         
         mp.put("judul", judul);
         mp.put("konten", konten);
@@ -101,6 +107,8 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idPost
+     * @return 
      */
     @WebMethod(operationName = "deletePost")
     public Boolean deletePost(@WebParam(name = "idPost") String idPost) {
@@ -112,11 +120,13 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idPost
+     * @return 
      */
     @WebMethod(operationName = "publishPost")
     public Boolean publishPost(@WebParam(name = "idPost") String idPost) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();
+        Map<String,Object> mp = new HashMap<>();
         mp.put("published",1);
         fb.child("posts").child(idPost).updateChildren(mp);
         return true;
@@ -124,21 +134,29 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param nama
+     * @param email
+     * @param role
+     * @param password
+     * @return 
      */
     @WebMethod(operationName = "addUser")
     public Boolean addUser(@WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "role") String role, @WebParam(name = "password") String password) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();
+        Map<String,Object> mp = new HashMap<>();
+        Map<String,Object> mp2 = new HashMap<>();
+        mp2.put(nama, "yeah");
         mp.put("nama", nama);
         mp.put("email", email);
         mp.put("role",role);
         mp.put("password", password);
-        fb.child("users").push().setValue(mp);
+        fb.child("users").child(nama).setValue(mp);
         return true;
     }
 
     /**
      * Web service operation
+     * @return 
      */
     @WebMethod(operationName = "listUser")
     public String listUser() {
@@ -166,11 +184,17 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idUser
+     * @param nama
+     * @param role
+     * @param email
+     * @param password
+     * @return 
      */
     @WebMethod(operationName = "editUser")
     public Boolean editUser(@WebParam(name = "idUser") String idUser, @WebParam(name = "nama") String nama, @WebParam(name = "role") String role, @WebParam(name = "email") String email, @WebParam(name = "password") String password) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();
+        Map<String,Object> mp = new HashMap<>();
         
         mp.put("nama", nama);
         mp.put("email", email);
@@ -182,6 +206,8 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idUser
+     * @return 
      */
     @WebMethod(operationName = "deleteUser")
     public Boolean deleteUser(@WebParam(name = "idUser") String idUser) {
@@ -192,11 +218,16 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param nama
+     * @param email
+     * @param konten
+     * @param idPost
+     * @return 
      */
     @WebMethod(operationName = "addComment")
     public Boolean addComment(@WebParam(name = "nama") String nama, @WebParam(name = "email") String email, @WebParam(name = "konten") String konten, @WebParam(name = "idPost") String idPost) {
         //TODO write your implementation code here:
-        Map<String,Object> mp = new HashMap<String,Object>();  
+        Map<String,Object> mp = new HashMap<>();  
         mp.put("nama", nama);
         mp.put("email", email);
         mp.put("tanggal",new Date());
@@ -207,6 +238,8 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param idPost
+     * @return 
      */
     @WebMethod(operationName = "listComment")
     public String listComment(@WebParam(name = "idPost") String idPost) {
@@ -233,6 +266,9 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param id
+     * @param idPost
+     * @return 
      */
     @WebMethod(operationName = "deleteComment")
     public Boolean deleteComment(@WebParam(name = "id") String id,@WebParam(name = "idPost") String idPost) {
@@ -243,6 +279,8 @@ public class cobablog {
 
     /**
      * Web service operation
+     * @param query
+     * @return 
      */
     @WebMethod(operationName = "dummyTest")
     public List search(@WebParam(name = "query") String query) {
@@ -250,4 +288,18 @@ public class cobablog {
         return null;
     }
 
+    /**
+     * Web service operation
+     * @param idPost
+     * @return 
+     */
+    @WebMethod(operationName = "softDelete")
+    public Boolean softDelete(@WebParam(name = "idPost") String idPost) {
+        //TODO write your implementation code here:
+        Map<String, Object> mp = new HashMap<>();
+        mp.put("deleted","1");
+        fb.child("posts").child(idPost).updateChildren(mp);
+        return true;
+    }
+   
 }
